7d1a89e4cbd7daa510a60319c2a5b712422457dd
spencerdodd <spencer.evan.dodd@gmail.com>
2018-02-07 23:36:48 -0500

    updated script echoes

--- letmein.sh
+++ letmein.sh
@@ -15,25 +15,25 @@ echo "[+] complete"
 
 # open port 22
 echo "[*] enabling ssh server on port 31337 with pubkey authentication,"
-echo "	no password login, no root login, and 3 login attempts"
+echo "	no password authentication, no root login, and 3 login attempts"
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sudo sed -e 's/#Port 22/Port 31337/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
 
 # enable pubkey auth
+echo "[*] (sshd_config) enabling public key authentication"
 sudo sed -e 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
-echo "[+] complete"
 
 # disable password login
+echo "[*] (sshd_config) disabling password authentication"
 sudo sed -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
-echo "[+] complete"
 
 # disable root login
+echo "[*] (sshd_config) disabling root login"
 sudo sed -e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
-echo "[+] complete"
 
 # lock out after 3 tries
+echo "[*] (sshd_config) setting login attempt lockout at 3 attempts"
 sudo sed -e 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
-echo "[+] complete"
 
 # restart ssh
 echo "[*] restarting ssh to enable changes"
