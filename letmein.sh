7b88bf84169e364a8974043e7338f93a67ee0dde
spencerdodd <spencer.evan.dodd@gmail.com>
2018-02-07 23:03:59 -0500

    updated pub key. removed rsub. changed ssh port to 31337. added blank sshd_config for test purposes

--- letmein.sh
+++ letmein.sh
@@ -6,28 +6,20 @@ sudo apt-get --force-yes --yes install openssh-server
 echo "[+] complete"
 
 # my 4096-bit ssh pubkey
-macbook="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCcQmcqDoQji+0NYe24CY1CeppIeqd9w0WIx3DSUe/CPtkl/ihtIH7L1ePxhroxixtcddfD0Hn6sD6DKJiirPE2PT3R7aUvpLAnYS4ToAE7W8uM19ISyrzLUUudeJAqwYApr8wYdr2/Uk8S+0OphFXfxr2qEmSXB8iO4pynFlVf7o5iC0dVHSK/cQ1K9TxXzXWKUY9b6fuUm/ARzprrJVtf6xFpaap37KgfFbnNlRQoQw9YtSe1ORbtOeU/++1VMUjr5ShhU4pLLE3VuJsX5v4jKRp9L86OlgopjKBMdqFEaDXqw0FrKNw88NptuFffqLv8Z6c1gtVch2Fmi+9DnuruJd/alKO/rgjflWF7Mt2DHSPTvVCX7fVEQ8rbtDoQ1VaEroDV7nyFUAy2md238pmP4fcsHb1xRdzd4sC9DPFj+ln2M3E++Jqsi/ByScpU6S7FTB5rsmnfN/9+AzSt6sSVotueHLsfZOzIakkXNt5HSSYVbol9sGFL4zUDTR1s3pykStQjdsVIDibg4oFdA90t/9YZHEY8yY9zDSjXWOzCRmLYpAc35BAFPc996+HQVtZJcZKdzO79DaD37vZlI6tOGM2YQzyVDg82idX98w2xZs8j+hXBo98BhdTb1IEsX03uhszSzxgYgxogUiOKzVOdETAapoWAXQE56RgQSlvN9w== sd"
-blackbox="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCs2TQwIpO6wh9zv80prW0GXCdrvHZTs5hGFTNiLJYH5QQ/cEvPFcqgl9BDDauPmcP/hKD8gI6vIi07H3xprUr5l6sCky46Y8h8FJaedqECtstRSpIR221YqJpP9e36FJENbX44wl8lftJmT8chf9g+J3Y2lYObkD8zcL6me9bR7BlUAhEhP058ZYzahytHFPxgxMAdcV0YQMgypxrr005yvEZ0UbSCeb/3J+fQpOSAeshYqrC/0Wg++YObbrr8IrVT+6OJHdXzhgczKfTrffUeg5hFmNycs+7aRh1oUufZS/QV9vNY74nFpzWlmdI2m+g2+6JA+wsfp+aU5eiACxiYP2cvnC/I9X8bBajkDd4ww7C03zy87zOzKg4m63GwvTYoaZp6AAsErNrRsqAw7uPhFTUOHvUTmHtQgQ1rU6fntcMmQnmLmgnrG+ZLgmlXnBGFnd5HaY0RgBI4LRQuoOukFCf1fj54Dz/Enz9UDxi+X7Ml4SJ78tRToiGFKJuy6nYvFUzPgFHa+jSeph8a4pXlHuFTXp7zU1pWM32cQplATRAEtZM0IHKhleel744sD6lwN9xmnPCXp5GRjg21t5nACpNc/pPaXxtPCKNyEX/63q2vuMoK5CTn+THJep8cmiVx1HNThVKwuaKd4zLLEgshhDm5mdu4vScikQZEWw/GLQ== coastal@blackbox"
+macbook="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/wDmMzRBwyxFmU8dTCHTF3s4elnThXELbbeo9XYDRSq73h4TL1fORottKvFQQgj+kpwnLopXN3U0/zSKncNrMJGvk5VvxgSUdyIfqyjCfISoh1AMk6UJOklENTIS7X/PyNX5gTnIpd/VAm5UQ/BvTVh5VaS0+Rc6I4AnuXm9gKLTlwtRpHrTJXlILznOPUaiz1uH2VSapY71ipPXyIHBcMi2u4qVKISOu3OG+t0L2UpUKpzsu27TtdussLGs1zWxW2TIAB1HBkSgfoOhTEiEzzB0K8DTVd2jHT9wv+pPBDiti2LH3pire5vQlrgNFVaMMC0cJiSJmYWK5VaEUr9bVT+2QGQpi+Wkg49xEOj99AQ+99BL/J0scFh8LGPOGxOZajUkjUbzypNxNkRBBuj/5al5ZV4KYtyURi/GbWdEc4usfsyOXJSyZv818wfshNl5u6UmcsPNpJQtTMtwXF+yGVL0BocpXH2HQYz+i8fFbe/NnwC1TuxhYmhviVZL22c79KvIyBgNheQ6Xhi+1MzMgMNQpYwMjZ9UpvvgLxjOjcgYUTe6mgFG5mK5wGz/INGZPkTRwvo3RsykLsWl52eu+U1IAPlVeaLfBXqZstwuydd36Z4/HJqmLtoo1v4g9h30UeClGt7wkng4zh1x+fVi2QdeF1QZUE/y73lj3AdUrSw== sd"
 
 # copy key to authorized_keys file
 echo "[*] copying coastal's ssh pubkey to ~/.ssh/authorized_keys"
 sudo echo $macbook >> ~/.ssh/authorized_keys
-sudo echo $blackbox >> ~/.ssh/authorized_keys
 echo "[+] complete"
 
 # open port 22
-echo "[*] enabling ssh server on port 22 with pubkey authentication"
+echo "[*] enabling ssh server on port 31337 with pubkey authentication"
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sudo sed -e 's/#Port/Port/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
+sudo sed -e 's/#Port 22/Port 31337/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
 
 # enable pubkey auth
 sudo sed -e 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config > /tmp/ssh_tmp; sudo mv /tmp/ssh_tmp /etc/ssh/sshd_config
-echo "[+] complete"
-
-# install rsub
-echo "[*] installing rsub"
-sudo wget -O /usr/local/bin/rsub https://raw.github.com/aurora/rmate/master/rmate
-sudo chmod +x /usr/local/bin/rsub
 echo "[+] complete"
 
 # restart ssh
